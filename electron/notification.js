363472eb4a86b5bfd1d0e3f2954b18b90bd6189a
EduardooPV <eduardoveltroni@hotmail.com>
2022-11-07 17:13:20 -0300

    refactor: folders and files

--- electron/notification.js
+++ electron/notification.js
@@ -2,7 +2,8 @@ const NOTIFICATION_TITLE = "Title";
 const NOTIFICATION_BODY =
   "Notification from the Renderer process. Click to log to console.";
 
-
 function showNotification() {
-  new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY, icon: './assets/notification.png'});
+  new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY, icon: './assets/notification.png' });
 }
+
+module.exports = showNotification
